59a988403794292773ea539384bf27a55e6b3933
Rodrigo <devcampanella@gmail.com>
2025-06-29 17:46:53 -0300

    teste negativos e hooks

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,9 +1,12 @@
 /// <reference types="cypress"/>
 
-describe=('funcionalidade de login', () => {
+describe('funcionalidade de login', () => {
 
+        beforeEach(() => {
+            cy.visit('https://www.lojaebac.ebaconline.art.br/minha-conta/')
+            
+        });
     it('Deve fazer login com sucesso', () => {
-        cy.visit('https://www.ebac.shop/minha-conta/')
         cy.get('#username').type('rodrigo.testee@testr.coom.br')
         cy.get('#password').type('123456')
         cy.get('.woocommerce-form > .button').click()
@@ -12,5 +15,17 @@ describe=('funcionalidade de login', () => {
 
     })
       
-      
+     it('Deve exibir uma menssagem de erro ao inserir usuario invalido', () => {
+
+        cy.get('#username').type('rodrigo.testee@testr.coom.br')
+        cy.get('#password').type('123456')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail')
+        cy.get('.woocommerce-error').should('existe')
+     }); 
+
+     it.only('deve exibir uma mensagem de erro', () => {
+        
+     });
 })
